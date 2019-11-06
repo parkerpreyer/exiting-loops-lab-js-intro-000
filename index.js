19dd98e6ff68109c7e7a5439092ca6b3c63b9730
Parker Preyer <parker.preyer@gmail.com>
2019-11-06 02:23:39 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function breakOut(array, changeValue, stopValue) {
+  for (var i = 0; i < array.length; i++) {
+    if(array[i] !== stopValue) {
+      array[i] = changeValue;
+    } else {
+      break;
+    }  
+  }
+  return array;
+}
